b4148ccd9cac299794b6ac5905a448524b876bd5
Rasheed Salau <32721902+BigLau14@users.noreply.github.com>
2024-04-12 16:49:18 -0500

    Initial commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,124 @@
+//let userMessage = "Hello World!";
+
+//console.log(userMessage);
+//1console.log(userMessage);
+
+//console.log(10 === 10);
+
+// function createGreeting(userName, message = "Hello!") {
+//     //console.log(userName);
+//     //console.log(message);
+//     return "Hi, I am " + userName + ". " + message;
+// }
+
+// const greeting1 = createGreeting("Rasheed")
+// console.log(greeting1);
+// const greeting2 = createGreeting("Corey", "Hey, what's up!");
+// console.log(greeting2);
+
+// const user = {
+//     name: 'Rasheed',
+//     age: 33,
+//     greet() {
+//         console.log("Hello!");
+//         console.log(this.age);
+//     }
+// };
+
+// console.log(user);
+// user.greet();
+
+// const hobbies = ["Sports", "Cooking", "Reading"];
+// console.log(hobbies[0]);
+
+// hobbies.push("Working");
+// console.log(hobbies);
+
+// const index = hobbies.findIndex (item => item === "Sports");
+
+// console.log(index);
+
+// const editedHobbies = hobbies.map((item) => ({text: item}));
+// console.log(editedHobbies);
+
+//const [firstName, lastName] = ["Rasheed", "Salau"];
+// const firstName = userNameData[0];
+// const lastName = userNameData[1];
+
+// console.log(firstName);
+// console.log(lastName);
+
+// const hobbies = ["Sports", "Cooking"];
+// const user = {
+//     name: "Rasheed",
+//     age: 33
+// }
+
+// console.log(hobbies);
+// const newHobbies = ["Reading"];
+// console.log(newHobbies);
+// const mergedHobbies = [...hobbies, ...newHobbies];
+// console.log(mergedHobbies);
+
+// const extendedUser = {
+//     idAdmin: true,
+//     ...user
+// };
+
+// console.log(extendedUser);
+
+//const password = prompt('Your Pasword');
+
+// if(password === "Hello"){
+//     console.log("Hello works");
+// } else if(password === "hello"){
+//     console.log("hello works");
+// } else {
+//     console.log("Access not granted");
+// }
+
+// const hobbies = ["Sports", "Cooking"];
+// for(const hobby of hobbies){
+//     console.log(hobby);
+// }
+
+// function handleTimeout(){
+//     console.log("Timed out!");
+// }
+
+// const handleTimeout2 = () => {
+//     console.log("Timed out ... again!");
+// }
+
+// setTimeout(handleTimeout, 2000);
+// setTimeout(handleTimeout2, 3000);
+// setTimeout(() => {
+//     console.log('More timing out...')
+// }, 4000);
+
+// function greeter(greetFn){
+//     greetFn();
+// }
+
+// greeter(() => console.log("Hi"));
+
+// function init() {
+//     function greet(){
+//         console.log("Hi!");
+//     }
+
+//     greet();
+// }
+
+// init();
+
+// let userMessage = "Hello!";
+// console.log(userMessage)
+// userMessage = "Hello there!";
+// console.log(userMessage)
+// userMessage = userMessage.concat("!!");
+// console.log(userMessage)
+
+const hobbies = ["Sprots", "Cooking"];
+hobbies.push("Working");
+console.log(hobbies);
